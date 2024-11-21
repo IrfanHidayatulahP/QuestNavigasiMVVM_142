d32d34d21b64364affdd7f7331d7a8552c2bf8d0
IrfanHidayatulahP <irfanhputra400@gmail.com>
2024-11-21 21:01:55 +0700

    menambahkan variable di string.xml dan pembuatan Data Siswa

---
+++ app/src/main/java/com/example/questnavigasimvvm_142/models/DataSiswa.kt
@@ -0,0 +1,10 @@
+package com.example.questnavigasimvvm_142.models
+
+data class DataSiswa(
+    val nim: String = "",
+    val nama: String = "",
+    val alamat: String = "",
+    val gender: String = "",
+    val email: String = "",
+    val notelepon: String = ""
+)
---
+++ app/src/main/java/com/example/questnavigasimvvm_142/models/JenisKelamin.kt
@@ -0,0 +1,7 @@
+package com.example.questnavigasimvvm_142.models
+
+import com.example.questnavigasimvvm_142.R
+
+object JenisKelamin {
+
+}
