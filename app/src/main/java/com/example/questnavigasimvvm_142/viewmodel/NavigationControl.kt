5f9d374542e264dcd1141e6b6ea414059ef965e8
IrfanHidayatulahP <irfanhputra400@gmail.com>
2024-11-21 21:29:26 +0700

    Membuat fungsi NavigationControl dan menambahkan fungsi nim di saveDataSiswa

--- app/src/main/java/com/example/questnavigasimvvm_142/viewmodel/NavigationControl.kt
+++ app/src/main/java/com/example/questnavigasimvvm_142/viewmodel/NavigationControl.kt
@@ -1,8 +1,53 @@
 package com.example.questnavigasimvvm_142.viewmodel
 
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.getValue
+import androidx.compose.ui.platform.LocalContext
+import androidx.lifecycle.viewmodel.compose.viewModel
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+import com.example.questnavigasimvvm_142.models.JenisKelamin
+import com.example.questnavigasimvvm_142.ui.view.FormulirView
+import com.example.questnavigasimvvm_142.ui.view.TampilDataView
 
 
 enum class Halaman {
     FORMULIR,
     TAMPILDATA
+}
+
+@Composable
+fun NavigationControl(
+    viewModel: SiswaViewModel = viewModel(),
+    navHost: NavHostController = rememberNavController(),
+    modifier: Any
+) {
+    val uiState by viewModel.statusUI.collectAsState()
+    NavHost(
+        navController = navHost,
+        startDestination = Halaman.FORMULIR.name
+    ) {
+        composable(
+            route = Halaman.FORMULIR.name
+        ){
+            val konteks = LocalContext.current
+            FormulirView(
+                listJK = JenisKelamin.JenisK.map { id -> konteks.getString(id) },
+                onSubmitClicked = {
+                    viewModel.saveDataSiswa(it)
+                    navHost.navigate(Halaman.TAMPILDATA.name)
+                }
+            )
+        }
+
+        composable(route = Halaman.TAMPILDATA.name) {
+            TampilDataView(
+                uiState = uiState,
+                onBackButton = { navHost.popBackStack() }
+            )
+        }
+    }
 }
--- app/src/main/java/com/example/questnavigasimvvm_142/viewmodel/SiswaViewModel.kt
+++ app/src/main/java/com/example/questnavigasimvvm_142/viewmodel/SiswaViewModel.kt
@@ -1,23 +1,25 @@
 package com.example.questnavigasimvvm_142.viewmodel
 
+import androidx.lifecycle.ViewModel
 import com.example.questnavigasimvvm_142.models.DataSiswa
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.flow.asStateFlow
 import kotlinx.coroutines.flow.update
 
-class SiswaViewModel {
+class SiswaViewModel : ViewModel(){
     private val _statusUI = MutableStateFlow(DataSiswa())
     val statusUI: StateFlow<DataSiswa> = _statusUI.asStateFlow()
 
     fun saveDataSiswa(ls: MutableList<String>) {
         _statusUI.update { statusSaatIni ->
             statusSaatIni.copy(
-                nama = ls[0],
-                gender = ls[1],
-                email = ls[2],
-                alamat = ls[3],
-                notelepon = ls[4]
+                nim = ls[0],
+                nama = ls[1],
+                gender = ls[2],
+                email = ls[3],
+                alamat = ls[4],
+                notelepon = ls[5]
             )
         }
     }
