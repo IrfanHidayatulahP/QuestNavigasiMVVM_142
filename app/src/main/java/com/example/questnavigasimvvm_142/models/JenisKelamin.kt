839220313ad861e9180ba4e5d878b24106475c6e
IrfanHidayatulahP <irfanhputra400@gmail.com>
2024-11-21 21:02:46 +0700

    Membuat object untuk JenisKelamin

--- app/src/main/java/com/example/questnavigasimvvm_142/models/JenisKelamin.kt
+++ app/src/main/java/com/example/questnavigasimvvm_142/models/JenisKelamin.kt
@@ -3,5 +3,8 @@ package com.example.questnavigasimvvm_142.models
 import com.example.questnavigasimvvm_142.R
 
 object JenisKelamin {
-
+    val JenisK = listOf(
+        R.string.laki,
+        R.string.wanita
+    )
 }
