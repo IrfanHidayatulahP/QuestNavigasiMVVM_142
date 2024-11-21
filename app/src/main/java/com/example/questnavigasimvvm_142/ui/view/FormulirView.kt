e0c8dc9de7d0eaecf0f3cc22012dda2f2ffe9af8
IrfanHidayatulahP <irfanhputra400@gmail.com>
2024-11-21 21:10:54 +0700

    Membuat desain tampilan untuk FormulirView

--- app/src/main/java/com/example/questnavigasimvvm_142/ui/view/FormulirView.kt
+++ app/src/main/java/com/example/questnavigasimvvm_142/ui/view/FormulirView.kt
@@ -1,10 +1,24 @@
 package com.example.questnavigasimvvm_142.ui.view
 
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.material3.Button
+import androidx.compose.material3.RadioButton
+import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.input.KeyboardType
+import androidx.compose.ui.unit.dp
 
 @Composable
 fun FormulirView(
@@ -19,6 +33,85 @@ fun FormulirView(
     var gender by remember { mutableStateOf("") }
 
     val listData : MutableList<String> = mutableListOf(nim, nama, gender, email, alamat, notelepon)
+
+    Column(
+        modifier = Modifier.fillMaxSize()
+            .padding(16.dp),
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        TextField(
+            value = nama,
+            onValueChange = {nama = it},
+            label = {
+                Text(text = "nama")
+            },
+            placeholder = {
+                Text(text = "Isi Nama Anda")
+            },
+            modifier = Modifier.fillMaxWidth()
+                .padding(5.dp)
+        )
+
+        Row (
+            modifier = Modifier.fillMaxWidth()) {
+            listJK.forEach{selectedGender ->
+                Row (verticalAlignment = Alignment.CenterVertically) {
+                    RadioButton(
+                        selected = gender == selectedGender,
+                        onClick = {
+                            gender = selectedGender
+                        })
+                }
+                Text(text = selectedGender) }
+        }
+
+        TextField(
+            value = email,
+            onValueChange = {email = it},
+            label = {
+                Text(text = "Email")
+            },
+            placeholder = {
+                Text(text = "Isi Email Anda")
+            },
+            modifier = Modifier.fillMaxWidth()
+                .padding(5.dp),
+            keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Email)
+        )
+
+        TextField(
+            value = alamat,
+            onValueChange = {alamat = it},
+            label = {
+                Text(text = "Alamat")
+            },
+            placeholder = {
+                Text(text = "Isi Alamat Anda")
+            },
+            modifier = Modifier.fillMaxWidth()
+                .padding(5.dp)
+        )
+
+        TextField(
+            value = notelepon,
+            onValueChange = {notelepon = it},
+            label = {
+                Text(text = "No Telepon")
+            },
+            placeholder = {
+                Text(text = "Isi No Telepon Anda")
+            },
+            modifier = Modifier.fillMaxWidth()
+                .padding(5.dp),
+            keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Number)
+        )
+
+        Button(onClick = {
+            onSubmitClicked(listData)
+        }) {
+            Text(text = "Simpan")
+        }
+    }
 }
 
 
